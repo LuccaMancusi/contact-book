36f7bfdb7d9ae09f194b38d171844711116886b9
LuccaMancusi <luccaromano@gmail.com>
2024-09-29 11:40:14 -0300

    feat: implements `unmark_favorite_contact` function

--- contacts.py
+++ contacts.py
@@ -6,6 +6,8 @@ class Contact:
         self.favorite = favorite
     def mark_as_favorite(self):
         self.favorite = True
+    def remove_favorite_mark(self):
+        self.favorite = False
 
 def append_contact(contacts):
     name = input("Digite o nome do novo contato: ")
@@ -36,8 +38,14 @@ def mark_favorite_contact(contacts):
         contacts[contact_index - 1].mark_as_favorite()
         print(f"\nContato {contact_index} marcado como favorito!")
 
-def unmark_favorite_contact():
-    pass
+def unmark_favorite_contact(contacts):
+    contact_index = int(input("\nDigite o id do contato que deseja desmarcar como favorito: "))
+    if (contact_index < 1 or contact_index > len(contacts)):
+        print("Este id não é válido! ")
+        return
+    else:
+        contacts[contact_index - 1].remove_favorite_mark()
+        print(f"\nContato {contact_index} desmarmarcado como favorito!")
 
 def show_favorite_contacts():
     pass
