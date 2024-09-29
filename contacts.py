05fc05392bc3dfef1cf4158cd1ceab8de4eea836
LuccaMancusi <luccaromano@gmail.com>
2024-09-29 11:34:35 -0300

    feat: implements `mark_favorite_contact` function

--- contacts.py
+++ contacts.py
@@ -4,6 +4,8 @@ class Contact:
         self.phone = phone
         self.email = email
         self.favorite = favorite
+    def mark_as_favorite(self):
+        self.favorite = True
 
 def append_contact(contacts):
     name = input("Digite o nome do novo contato: ")
@@ -25,8 +27,14 @@ def show_contacts(contacts):
         print(f"Favorito: {msg}")   
     return
 
-def mark_favorite_contact():
-    pass
+def mark_favorite_contact(contacts):
+    contact_index = int(input("\nDigite o id do contato que deseja marcar como favorito: "))
+    if (contact_index < 1 or contact_index > len(contacts)):
+        print("Este id não é válido! ")
+        return
+    else:
+        contacts[contact_index - 1].mark_as_favorite()
+        print(f"\nContato {contact_index} marcado como favorito!")
 
 def unmark_favorite_contact():
     pass
